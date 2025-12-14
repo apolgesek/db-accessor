f508a0be3aa6c28db17452e2bc1f6a08aa55716f
apolgesek <iceman129@gmail.com>
2025-12-14 11:29:27 +0100

    fix: name

--- src/functions/get_active_policies/sso/main.ts
+++ src/functions/get_active_policies/sso/main.ts
@@ -60,7 +60,7 @@ class LambdaHandler {
           );
 
           const ps = desc.PermissionSet;
-          if (ps?.Name && ps.Name.startsWith('SSOdynamodb_GetItemPolicy')) {
+          if (ps?.Name && ps.Name.startsWith('SSO')) {
             return { arn, name: ps.Name, creationDate: ps.CreatedDate };
           }
 
