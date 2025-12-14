bcc8de832933c22731ab10580236490008f2152b
apolgesek <iceman129@gmail.com>
2025-12-14 23:48:49 +0100

    fix: delete

--- src/functions/delete_expired_policies/iam/main.ts
+++ src/functions/delete_expired_policies/iam/main.ts
@@ -27,7 +27,7 @@ class LambdaHandler {
       );
 
       for (const policy of res.Policies || []) {
-        if (policy.PolicyName?.startsWith('dynamodb_GetItemPolicy')) {
+        if (policy.PolicyName?.startsWith('IAM')) {
           matched.push(policy);
         }
       }
